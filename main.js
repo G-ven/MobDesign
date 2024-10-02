b0ff4760c0c43d67d089384459a2990670455726
G-ven <162297245+G-ven@users.noreply.github.com>
2024-10-02 12:32:06 +0200

    Ajout d'un menu déroulant en js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-import './assets/scss/style.scss'
+import '/assets/scss/style.scss';
 
+// MENU 
+function openNav(event) {
+    this.classList.toggle("close");
+    const menu = document.querySelector(".js-open-menu");
+    menu.classList.toggle("open");
+    console.log(menu);
+}
 
-
-
+document.querySelector("#header-btn").addEventListener('click', openNav);
